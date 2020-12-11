8c8063c8aa4cbc8f2555dec1b58fa84359696c6a
ab-ashik <75864062+ab-ashik@users.noreply.github.com>
2020-12-12 05:22:39 +0600

    UPDATE-2

--- StartAccount.java
+++ StartAccount.java
@@ -17,3 +17,67 @@ public class StartAccount
 		
 	}
 }
+
+
+
+public class AccountUpgrade
+{
+	private String accId;
+    private String accName;
+    private int accBalance;
+	public AccountUpgrade(){}
+	public AccountUpgrade(String accName, String accId,int accBalance){
+		this.accName=accName;
+		this.accId=accId;
+		this.accBalance=accBalance;
+	}
+	public void setAccName(String accName){this.accName=accName;}
+	public void setAcId(String accId){this.accId=accId;}
+	public void setBalance(int accBalance){this.accBalance = accBalance;}
+	public String getAccName(){return accName;}
+	public String getAccId(){return accId;}
+	public int getBalance(){return accBalance;}
+	public int deposite(int amount){
+		accBalance +=amount;
+		System.out.println("Your deposite amount : "+amount);
+		System.out.println("Balance : "+accBalance);
+		return accBalance;
+	}
+	public int withdraw(int amount){
+		accBalance -=amount;
+		System.out.println("Your withdraw amount : "+amount);
+		System.out.println("Balance : "+accBalance);
+		return accBalance;
+	}
+	public void showInfo()
+	{
+		System.out.println("Account Name :" + accName);
+		System.out.println("Account id :" + accId);
+		System.out.println("Balance :" + accBalance);
+	}
+	
+	static Transaction transaction[] = new Transaction[100] ;
+	static int totalNumberOfTransaction = 0;
+
+	public void addTransaction(AccountUpgrade sender, AccountUpgrade receiver, int amount)
+	{
+		transaction[totalNumberOfTransaction] = new Transaction(sender , receiver,  amount);
+		totalNumberOfTransaction++;
+	}
+
+	public void transfer(int amount, AccountUpgrade receiver){
+		addTransaction(this, receiver, amount);
+		int y = receiver.deposite(amount);
+		System.out.println("Transfer Completed");
+		System.out.println("Balance of "+this.accName+" is: "+this.accBalance);
+		System.out.println("Balance of "+receiver.accName+" is: "+receiver.accBalance);
+	}
+	public void showAlltransaction()
+	{
+		for(int i = 0; i<totalNumberOfTransaction; i++)
+		{
+			transaction[i].showTransactionInfo();
+		}	
+	}
+	
+}
